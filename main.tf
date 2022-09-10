b907ec00a4cd011eb9f58e9f4244ebf35e9c6532
Stefan Dieni <stefan@dieni.com>
2022-09-10 11:24:46 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,100 @@
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "main_public_subnet" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev_public"
+  }
+}
+
+resource "aws_internet_gateway" "main_internet_gateway" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "dev_igw"
+  }
+}
+
+resource "aws_route_table" "main_public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.main_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main_internet_gateway.id
+}
+
+resource "aws_route_table_association" "main_public_association" {
+  subnet_id      = aws_subnet.main_public_subnet.id
+  route_table_id = aws_route_table.main_public_rt.id
+}
+
+resource "aws_security_group" "main_sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["88.97.6.230/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "main_auth" {
+  key_name   = "mainkey"
+  public_key = file("~/.ssh/mainkey.pub")
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.main_server_ami.id
+  key_name               = aws_key_pair.main_auth.id
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  subnet_id              = aws_subnet.main_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      # ${var.host_os} refers to a variable defined in variables.tf so that the correct ssh-config.tpl is used (VSCode running on windows vs linux)
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/mainkey"
+    })
+    interpreter = var.host_os == "linux" ? ["bash", "-c"] : ["Powershell", "-Command"]
+    # if the host_os is linux as defined in variable.tf then the argument is true and the interpreter is bash -c otherwise it uses Powershell -Command
+  }
+}
